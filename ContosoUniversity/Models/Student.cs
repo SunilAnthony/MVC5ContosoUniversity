5e254f9a99ada2f810430231cdf6054bacc73fa1
SunilAnthony <zXSwordXz@hotmail.com>
2017-05-26 10:30:56 -0400

    Initial Commit

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Last Name:")]
+        [StringLength(50, MinimumLength =2, ErrorMessage ="Invalid")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
